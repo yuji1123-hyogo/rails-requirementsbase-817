acd790b9de5fa01954863ca7deaf6ae69bcc631b
LAPTOP-T546O2B1\yuji1 <yuji1123200113@gmail.com>
2025-08-21 22:38:25 +0900

    a

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -8,20 +8,33 @@ class Api::LikesController < ApplicationController
 
   def create
     book = Book.find(params[:book_id])
-    if book
-      @current_user.favorite_books << book
+    like = current_user.likes.build(book: book)
+    if like.save
+      render_success('書籍をお気に入り登録しました', { like: like_response(like) }, :created)
+    elsif like.errors[:user_id].any?
+      render_error('すでにお気に入り登録済みです', ['この書籍はお気に入り登録されています'])
     else
       render_error('お気に入り登録に失敗しました', book.errors, :bad_request)
     end
+  rescue ActiveRecord::RecordNotFound
+    render_error(
+      '書籍が見つかりません', ['指定された書籍は存在しません'], :not_found
+    )
   end
 
   def destroy
-    favorite = @current_user.favorite_books(book_id: params[:book_id])
-    if favorite
+    like = @current_user.favorite_books.find_by!(book_id: params[:book_id])
+    if like.destroy
       render_success('お気に入りを解除しました', {}, :ok)
     else
-      render_error('お気に入り解除に失敗しました', favorite.errors, :bad_request)
+      render_error('お気に入り解除に失敗しました', like.errors, :bad_request)
     end
+  rescue ActiveRecord::RecordNotFound
+    render_error(
+      'お気に入りが見つかりません',
+      ['指定された書籍はお気に入り登録されていません'],
+      :not_found
+    )
   end
 
   private
